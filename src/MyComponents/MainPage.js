ce26890d5e64e54ec7a80a3eacff8ea0e820db16
Rakesh Pawar <rpawar@innominds.com>
2022-08-25 11:51:01 +0530

    added header

---
+++ src/MyComponents/MainPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+export default function MainPage() {
+
+  return (
+    <>
+      <div className="main-container">
+        <h1>Vehicle List</h1>
+        <button className="add-vehiclebtn">+ Add Vehicle</button>
+        <div className="main-container-right">
+          <i className="fa fa-bars"></i>
+        </div>
+        <div className="info">
+          <i class="fa fa-info"></i>
+        </div>
+      </div>
+
+      {/*  */}
+      
+    </>
+  );
+}
